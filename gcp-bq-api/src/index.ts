f4a602aafddc287facac45d8591066979899f17e
yo16 <xxyo1xx@gmail.com>
2025-05-21 11:58:22 +0900

    set Access-Control-Allow-Origin

--- gcp-bq-api/src/index.ts
+++ gcp-bq-api/src/index.ts
@@ -6,7 +6,17 @@ import { BigQuery } from '@google-cloud/bigquery';
 import { v4 as uuidv4 } from 'uuid';
 import type { Request, Response } from 'express';
 
+const allowedOrigins = [
+  'https://kuronekotaiwan-matsuri.github.io',
+  'http://localhost:3000',
+];
+
 export const insertToBigQuery = async (req: Request, res: Response) => {
+  const origin = req.headers.origin || '';
+  if (allowedOrigins.includes(origin)) {
+    res.set('Access-Control-Allow-Origin', origin);
+  }
+  
   // ★ CORS対応：プリフライト（OPTIONS）リクエスト
   if (req.method === 'OPTIONS') {
     res.set('Access-Control-Allow-Origin', 'https://kuronekotaiwan-matsuri.github.io');
@@ -17,7 +27,7 @@ export const insertToBigQuery = async (req: Request, res: Response) => {
   }
 
   // ★ CORS対応：本リクエストにもヘッダー付与
-  res.set('Access-Control-Allow-Origin', '*');
+  //res.set('Access-Control-Allow-Origin', '*');
 
   const { uuid, stamp_id } = req.body;
   if (!uuid || !stamp_id) {
